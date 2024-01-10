43325a03c21f83fecfffd34ae5c3536ed581fb55
fehor-sebpo <fehor.ahmed@sebpo.com>
2024-01-10 20:47:18 +0600

    ok

---
+++ app/Http/Controllers/ClientTestimonialController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClientTestimonial;
+use Illuminate\Http\Request;
+
+class ClientTestimonialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ClientTestimonial $clientTestimonial)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ClientTestimonial $clientTestimonial)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ClientTestimonial $clientTestimonial)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ClientTestimonial $clientTestimonial)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,8 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+   // dd('sdfa');
+    return view('backend.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
